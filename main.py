ebbbbc66a4f17823b6a04495c30bd7dfd976130f
blackfrog <34157174+black-frog-language@users.noreply.github.com>
2018-07-08 15:06:24 +0800

    add main.py
    add an most original GUI

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+main =  tk.Tk()
+main.title('AutoMatiC++') 
+main.geometry('1200x800')
+new = tk.Label(main,text = "NEW!",bg = 'green',font = ("Arial",30),width = 10,height =2)
+new.pack()
+main.mainloop()
